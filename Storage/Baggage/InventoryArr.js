369d2ed643e0a6c7ee5284e9cc096079aa510f99
Maksi24816 <93403660+Maksi24816@users.noreply.github.com>
2025-01-19 00:23:49 +0200

    Add files via upload

---
+++ Storage/Baggage/InventoryArr.js
@@ -0,0 +1,29 @@
+const TypeInvList = [
+    null,null,null,null,null,null,
+    0,0,0,0,
+    null,null,null,null,null,
+    null,null,null,null,null
+]
+
+//Name,[]
+//Name,[Ситость]
+//Name,[TypeEqu]
+const InventoryArr = [
+    [
+        [
+            ["Stick.png",[]],
+            ["Stone.png",[]],
+            ["Grass.png",[]]
+        ]
+    ],
+    [
+        [
+            ["Bushes.png",[15]]
+        ]
+    ],
+    [
+        [
+            ["Axe.png",[0]]
+        ]
+    ]
+]
